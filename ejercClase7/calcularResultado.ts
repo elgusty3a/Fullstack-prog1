d29bb86ab51afcb59d8bc6829872a07fa9bbd691
Arias Gustavo <ariasgustavo3a@gmail.com>
2024-09-17 08:02:41 -0300

    calcuular resultado menu

---
+++ ejercClase7/calcularResultado.ts
@@ -0,0 +1,7 @@
+import * as rdl from 'readline-sync';
+
+let num1 = rdl.questionInt("Ingrese el primer número: ")
+let num2 = rdl.questionInt("Ingrese el segundo número: ")
+let opcion = rdl.questionInt("Ingrese el número de opcion\nIngrese 1 para sumar\nIngrese 2 para restar\nIngrese 3 para multiplicar\nIngrese 4 para dividir\n ");
+
+
