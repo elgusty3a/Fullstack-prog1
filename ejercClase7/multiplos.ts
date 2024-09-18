4afe37aa87f2cc4c67bfc639056566b1a18f58e5
Arias Gustavo <ariasgustavo3a@gmail.com>
2024-09-17 23:01:27 -0300

    cantidad de multiplos

---
+++ ejercClase7/multiplos.ts
@@ -0,0 +1,27 @@
+import * as rdl from 'readline-sync';
+
+let dividendo:number = rdl.questionInt("Ingrese el dividendo:\n");
+let divisor:number = rdl.questionInt("Ingrese el divisor:\n");
+
+function esMultiplo(dividendo:number,divisor:number):boolean{
+  let esMultiplo:boolean = false;
+  if(dividendo%divisor==0){
+    esMultiplo = true;
+  }
+  return esMultiplo;
+}
+function cantMultiplos(dividendo:number) :number{
+  let cantidad:number = 0;
+  for (let i = 1; i <= dividendo; i++){
+    if(esMultiplo(dividendo,i)){
+      console.log(`${i} es multiplo de ${dividendo}\n`);
+      cantidad++;
+    }
+  }
+  return cantidad;
+}
+
+// let resultado:boolean = esMultiplo(dividendo,divisor);
+let resultado:number = cantMultiplos(dividendo);
+
+console.log(resultado);
