62d0420abd8ca19098d79370ee09333c03808a53
Gustavo Arias <ariasgustavo3a@gmail.com>
2024-09-24 13:42:58 -0300

    nombres deseados

---
+++ ejercArreglos/nombresDeseados.ts
@@ -0,0 +1,11 @@
+import * as rdl from 'readline-sync';
+
+let dimension:number = rdl.questionInt("Ingrese el tamaño del arreglo: \n");
+let nombres:string[] = new Array(dimension);
+for (let i = 0; i < nombres.length; i++) {
+  nombres[i] = rdl.question(`Ingrese el nombre que quiere agregar en la posicion ${i}: `);
+}
+for (let i = 0; i < nombres.length; i++) {
+  console.log(`En la posicion ${i} se encuentra ${nombres[i]}`);
+  
+}
